408718ac92c746a6a7c2e7bff15e8624264acad7
stijnringeling <stijnringeling@newman.nl>
2016-02-04 18:25:32 +0100

    db_connect.inc.php can do all database connecting

--- inner join test.php
+++ inner join test.php
@@ -3,10 +3,7 @@
 <?php
 	//bestand met connectie info voor verbing met de server.
 	//variabelen initialiseren.
-	$server = "localhost";
-	$user = "ITchallenge";
-	$password = "ITchallenge";
-	$database = "ITchallenge";
+	include("db_connect.inc.php");
 	$query = 	"SELECT 
 			n.ID
 			n.achternaam
@@ -15,29 +12,16 @@
 			FROM NAW AS n
 			INNER JOIN Rescourse_test as rt
 			ON n.id = rt.id";
-if (!$db =mysql_connect($server, $user, $password)){
-	$boodschap = "<h2>De verbinding met de databaseserver is mislukt!</h2>";
-}
-else{
-	$boodschap = "<h2>De verbinding met de databaseserver is tot stand gebracht.</h2><br/>";
-	
-	if(!mysql_select_db($database)){
-		$boodschap .="... maar de database \"database\"kon niet worden gevonden!";
+	$resultaat = "";
+	if(!$resultaat = mysql_query($query, $db)){
+		echo "de query \"$query\" kon niet worden uitgevoerd!";
 	}
 	else{
-		$boodschap .="en de database \"$database\"is geselecteerd";
-		if(!$resultaat = mysql_query($query, $db)){
-			$boodschap .="</br>... maar de query \"$query\" kon niet worden uitgevoerd!";
-		}
-		else{
-			$boodschap .= "</br>De query \"$query\" is met succes uitgevoerd";
-		}
+		echo "De query \"$query\" is met succes uitgevoerd";
 	}
-}
 ?>
 </head>
 <body>
-	<?php echo $boodschap; ?></br>
 	<p>Deze gegevens zijngevonden: </p>
 	<hr>
 	<?php while(list($ID, $achternaam, $hond, $auto)= mysql_fetch_row($resultaat)){
