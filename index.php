fe4460299ff65ccede01597ba806f1c9b5c11b4b
Stijn Ringeling <stijnringeling@newman.brd>
2016-03-18 12:14:07 +0100

    fixing homepage

--- index.php
+++ index.php
@@ -57,7 +57,7 @@
 			
 		</script>
 		
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+		<!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>-->
 <script>
 $(document).ready(function(){
   
